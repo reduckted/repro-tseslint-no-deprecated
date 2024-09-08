22ce014ebc788d529c1151c3a5809c2caffe713d
reduckted <reduckted@outlook.com>
2024-09-08 20:15:51 +1000

    Additional examples

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { inject, ComponentFactoryResolver } from '@angular/core';
+import { Foo } from './foo';
 
 // no-deprecated is reported for this:
 const f1: ComponentFactoryResolver = new ComponentFactoryResolver();
 
 // But it is not reported for this:
 const f2: ComponentFactoryResolver = inject(ComponentFactoryResolver);
+
+// An import from another file is also not reported.
+const f3: Foo = inject(Foo);
+
+/**
+ * @deprecated
+ */
+class Bar {}
+
+// This is reported:
+const b: Bar = inject(Bar);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Reproduction for typescript-eslint/typescript-eslint/#9946
+
+https://github.com/typescript-eslint/typescript-eslint/issues/9946
